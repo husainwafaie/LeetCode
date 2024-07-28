31a503a52dca2ffa300047c3c27ef358102ff883
Husain Wafaie <husainwafaie@gmail.com>
2024-07-28 12:11:42 -0700

    Delete 0001-Two-Sum directory

--- 0001-Two-Sum/0001-Two-Sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        first = 0
-        dct = {}
-        for i in range(len(nums)):
-            dct[nums[i]] = i
-        for i in range(len(nums)):
-            if target - nums[i] in dct and dct[target - nums[i]] != i:
-                return [i, dct[target - nums[i]]]
