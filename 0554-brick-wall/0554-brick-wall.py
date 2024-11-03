7f07f44720ca39ae968f621198595793dede7cdb
Husain Wafaie <husainwafaie@gmail.com>
2024-11-02 17:18:31 -0700

    Time: 7 ms (71.14%), Space: 20.5 MB (94.83%) - LeetHub

--- 0554-brick-wall/0554-brick-wall.py
+++ 0554-brick-wall/0554-brick-wall.py
@@ -1,5 +1,19 @@
 class Solution:
     def leastBricks(self, wall: List[List[int]]) -> int:
+        # n = sum(wall[0])
+        # heap = []
+        # dct = {}
+        # for i in range(len(wall)):
+        #     heapq.heappush(heap, [wall[i][0], i, 1])
+        # ret = len(wall)
+        # for i in range(1, n):
+        #     count = 0
+        #     while heap and heap[0][0] == i:
+        #         item = heapq.heappop(heap)
+        #         count += 1
+        #         heapq.heappush(heap, [item[0] + wall[item[1]][item[2]], item[1], item[2] + 1]) 
+        #     ret = min(ret, len(wall) - count)
+        # return ret
         dct = {}
         for i in range(len(wall)):
             total = 0
@@ -11,6 +25,7 @@ class Solution:
         for key in dct.keys():
             if len(wall) - dct[key] < ret:
                 ret = len(wall) - dct[key]
+            #ret = min(ret, len(wall) - dct[key])
         return ret
         
         
